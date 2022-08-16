3d6f52fae12bb1583879f04f73b3c30fcd6cf92b
Chris Karr <chris@audacious-software.com>
2022-08-16 13:44:10 -0400

    Adding CircleCI build config file

---
+++ mooclet_engine/mooclet_engine/circle.py
@@ -0,0 +1,50 @@
+import os
+# set default environment variable before importing base settings
+# os.environ.setdefault('ENV_TYPE', 'local')
+
+from .base import *
+# from . import secure
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# INSTALLED_APPS += ('debug_toolbar', 'sslserver')
+
+# MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'circle_test',
+        'USER':     'root',
+        'PASSWORD': '',
+        'HOST':     'localhost',
+        'PORT':     '',
+    }
+}
+
+# For Django Debug Toolbar:
+INTERNAL_IPS = ()
+
+# DEBUG_TOOLBAR_CONFIG = {
+#    'INTERCEPT_REDIRECTS': False,
+# }
+
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000
+
+
+#CELERY
+#CELERY_BROKER_URL = secure.LOCAL_CELERY_BROKER_URL
