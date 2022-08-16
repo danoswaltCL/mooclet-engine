401a0366b351318d037fc509a6ab7b6556f29bd3
Chris Karr <chris@audacious-software.com>
2022-08-16 16:00:20 -0400

    Making settings import more flexible

--- mooclet_engine/mooclet_engine/settings/base.py
+++ mooclet_engine/mooclet_engine/settings/base.py
@@ -38,8 +38,13 @@ path.append(SITE_ROOT)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secure.SECRET_KEY
+SECRET_KEY = 'CHANGEMECHANGEME'
+
+try:
+	# SECURITY WARNING: keep the secret key used in production secret!
+	SECRET_KEY = secure.SECRET_KEY
+except AttributeError:
+	pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
